b00a6a9907f752b54e9cf1f13e1eb3fce8cae2d2
AkshatIITk <iitkakshat@gmail.com>
2024-07-11 16:11:37 +0530

    initialised prof server and admin prof server

--- cmd/admin.go
+++ cmd/admin.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/pclubiitk/fep-backend/middleware"
+	"github.com/pclubiitk/fep-backend/prof"
 	"github.com/pclubiitk/fep-backend/project"
 	"github.com/spf13/viper"
 )
@@ -19,6 +20,27 @@ func adminProjectServer() *http.Server {
 	engine.Use(gin.Logger())
 
 	project.AdminRouter(engine)
+
+	server := &http.Server{
+		Addr:         ":" + PORT,
+		Handler:      engine,
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+	}
+
+	return server
+}
+
+func adminProfServer() *http.Server {
+	PORT := viper.GetString("PORT.ADMIN.PROF")
+	engine := gin.New()
+	engine.Use(middleware.CORS())
+	engine.Use(middleware.Authenticator())
+	engine.Use(middleware.EnsureAdmin())
+	engine.Use(gin.Recovery())
+	engine.Use(gin.Logger())
+
+	prof.AdminRouter(engine)
 
 	server := &http.Server{
 		Addr:         ":" + PORT,
---
+++ cmd/prof.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pclubiitk/fep-backend/middleware"
+	"github.com/pclubiitk/fep-backend/prof"
+	"github.com/spf13/viper"
+)
+
+func profServer() *http.Server {
+	PORT := viper.GetString("PORT.PROF")
+	engine := gin.New()
+	engine.Use(middleware.CORS())
+	// engine.Use(middleware.Authenticator())
+	engine.Use(gin.Recovery())
+	engine.Use(gin.Logger())
+
+	// rc.CompanyRouter(engine)
+	// application.CompanyRouter(engine)
+	prof.ProfRouter(engine)
+
+	server := &http.Server{
+		Addr:         ":" + PORT,
+		Handler:      engine,
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+	}
+
+	return server
+}
